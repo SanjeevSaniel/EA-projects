33aa5e00832a64561658c1e7bd9013e96ac55bcb
SanjeevSaniel <sanjeevsaniel@outlook.in>
2022-08-05 01:28:06 +0530

    aaded script to auto change of message based on time

--- Digital-Clock/js/script.js
+++ Digital-Clock/js/script.js
@@ -28,6 +28,33 @@ let clock = () => {
   document.getElementById("clock-seconds").innerHTML = clockSeconds;
   document.getElementById("clock-period").innerHTML = clockPeriod;
   setTimeout(clock, 1000);
+
+  let morningMessage = "Grab Some Healthy Breakfast!!!";
+  let afternoonMessage = "Let's Have Some Lunch !!";
+  let eveningMessage = `Stop Yawning, Get Some Tea..
+        It's Just Evening!`;
+  let nightMessage = "Close Your Eyes And Go To Sleep";
+
+  let morningText = "Good Morning!! Wake Up !!";
+  let afternoonText = "Good Afternoon !! Take Some Sleep";
+  let eveningText = "Good Evening !!";
+  let nightText = "Good Night !!";
+
+  if (period === "AM") {
+    document.getElementById("greeting-message").innerHTML = morningMessage;
+    document.getElementById("greeting-text").innerHTML = morningText;
+  } else if (period === "PM") {
+    if (hours === 12 || 1 <= hours <= 3) {
+      document.getElementById("greeting-message").innerHTML = afternoonMessage;
+      document.getElementById("greeting-text").innerHTML = afternoonText;
+    } else if (4 <= hours <= 7) {
+      document.getElementById("greeting-message").innerHTML = eveningMessage;
+      document.getElementById("greetings-text").innerHTML = eveningText;
+    } else {
+      document.getElementById("greeting-message").innerHTML = nightMessage;
+      document.getElementById("greeting-text").innerHTML = nightText;
+    }
+  }
 };
 
 clock();
