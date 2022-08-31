21a1980c2edbedc9ac790724f30c9a9aef4b7bec
SanjeevSaniel <sanjeevsaniel@outlook.in>
2022-08-31 21:22:56 +0530

    Fixed the add task related issue

--- Todo-App-JS/js/old-script.js
+++ Todo-App-JS/js/old-script.js
@@ -1,3 +1,4 @@
+// let cardCount = 0;
 let data = [];
 let itemCount = 1;
 const cards = document.getElementById("cards");
@@ -37,6 +38,18 @@ const modal = () => {
   });
 };
 
+// let doneButton = document.querySelector(".btn-done");
+// doneButton.addEventListener(
+//   "click",
+//   function (e) {
+//     if (e.target.parentElement.tagName === "LI") {
+//       e.target.parentElement.classList.toggle("checked");
+//       e.target.classList.add("hide");
+//     }
+//   },
+//   false
+// );
+
 // Creation of Cards
 let acceptData = () => {
   let cardTitle = document.getElementById("list-title").value;
@@ -53,6 +66,36 @@ let acceptData = () => {
 };
 
 let createCards = () => {
+  // cards.innerHTML = "";
+  // let card = document.createElement("div");
+  // let cardID = `${++cardCount}`;
+
+  // cardTitle = (cardTitle[0].toUpperCase() + cardTitle.substring(1)).trim();
+  // let inputItem = document.getElementById("item").value;
+  // let listItems = document.getElementsByClassName("list-items");
+  // let li = document.createElement("li");
+  // card.setAttribute("class", cardID);
+
+  // card.classList.add("card");
+  // card.id = cardID;
+  // cards.appendChild(card);
+
+  // data.map((x, y) => {
+  //   return (card.innerHTML = `<span class="card__title" data-open="card-modal-list">${cardTitle}</a></span>
+  //         <hr class="card-hr" />
+  //         <br />
+  //         <ul class="items-list">
+  //         </ul>
+  //         <br /><br />
+  //         <div class="add-item-button" data-open="modal-list-2">
+  //           <img src="./images/icons8-plus-50.png" alt="">
+  //         </div>
+  //         <div class="delete-button" onclick="deleteCard(this)">
+  //           <img src="./images/bin.svg" alt="">
+  //         </div>
+  //         `);
+  // });
+
   data.map((x, y) => {
     return (cards.innerHTML += `<div id=${y} class="card">
         <span class="card__title" data-open="card-modal-list">${x.name}</a></span>
@@ -71,6 +114,32 @@ let createCards = () => {
     // return cards.appendChild(card);
   });
 
+  // Add Button ➕
+  // let addButton = document.createElement("div");
+  // addButton.setAttribute("class", "add-item-button");
+  // card.appendChild(addButton);
+  // let img1 = document.createElement("img");
+  // img1.src = "./images/icons8-plus-50.png";
+  // addButton.appendChild(img1);
+  // addButton.addEventListener("click", function () {
+  //   popup.style.display = "block";
+  // });
+
+  // console.log(data);
+
+  // Delete Button 🚮
+  // let deleteButton = document.createElement("div");
+  // deleteButton.setAttribute("class", "delete-button");
+  // card.appendChild(deleteButton);
+  // let img2 = document.createElement("img");
+  // img2.src = "./images/bin.svg";
+  // deleteButton.appendChild(img2);
+  // img2.addEventListener("click", function (e) {
+  //   e.parentElement.remove();
+  //   data.splice(e.parentElement.id, 1);
+  // console.log(data);
+  // });
+
   // Referenced for resolving functionalty error - https://stackoverflow.com/questions/70585031/cant-select-element-with-dom-after-creating-that-element-with-other-function
 
   modal();
@@ -84,6 +153,14 @@ const addItems = () => {
   li.id = `item-${itemCount++}`;
   li.innerHTML = `${itemName}<button class="btn-done" onclick="itemChecked(this)">done</button>`;
 };
+// const addItems = () => {
+//   let itemName = document.getElementById("list-item-title").value;
+//   let li = document.createElement("li");
+//   let ul = document.querySelector(".items-list");
+//   ul.appendChild(li);
+//   li.id = `item-${itemCount++}`;
+//   li.innerHTML = `${itemName}<button class="btn-done" onclick="itemChecked(this)">done</button>`;
+// };
 
 let itemChecked = (e) => {
   e.parentElement.classList.add("checked");
@@ -97,6 +174,17 @@ let deleteCard = (e) => {
   location.reload();
   console.log(data);
 };
+// let list = document.querySelector("ul");
+// let checkedButton = document.querySelector(".btn-done");
+// checkedButton.addEventListener(
+//   "click",
+//   function (ev) {
+//     if (ev.target.parentElement.tagName === "LI") {
+//       ev.target.classList.toggle("checked");
+//     }
+//   },
+//   false
+// );
 
 modal();
 (() => {
