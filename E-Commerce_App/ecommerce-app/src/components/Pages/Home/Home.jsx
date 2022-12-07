ebdb1231c528f7a7a9324d46a77a03e0bdb406dd
SanjeevSaniel <sanjeevsaniel@outlook.in>
2022-12-08 04:34:31 +0530

    Added new components

--- E-Commerce_App/ecommerce-app/src/components/Pages/Home/Home.jsx
+++ E-Commerce_App/ecommerce-app/src/components/Pages/Home/Home.jsx
@@ -1,36 +1,19 @@
 import React from "react";
-// import ReactPlayer from "react-player/lazy";
+import About from "../../Utilities/About/About";
+import Services from "../../Utilities/Services/Services";
 import "./Home.css";
 import BannerVideo from "./videos/AirPodsPro_Rebuiltfromthesoundup_Apple.mp4";
 
 const Home = () => {
-  // const video = document.getElementById("banner-video");
-  // const btn = document.getElementById("myBtn");
-
-  // const handleVideo = () => {
-  //   if (video.paused) {
-  //     video.play();
-  //     btn.innerHTML = "Pause";
-  //   } else {
-  //     video.pause();
-  //     btn.innerHTML = "Play";
-  //   }
-  // };
-
   return (
-    <div className="banner">
-      <video id="banner-video" src={BannerVideo} autoPlay loop muted></video>
-
-      {/* <div className="content">
-        <h1>Heading</h1>
-        <p>Lorem ipsum...</p>
-        <button id="myBtn" onclick={handleVideo}>
-          Pause
-        </button>
-      </div> */}
-      {/* <span className="banner-title">AirPods Pro</span> */}
-      {/* <ReactPlayer url={BannerVideo} playing={true} loop={true} /> */}
-    </div>
+    <React.Fragment>
+      <div className="banner">
+        <video id="banner-video" src={BannerVideo} autoPlay loop muted></video>
+      </div>
+      <Services />
+      <hr className="hr-one"/>
+      <About />
+    </React.Fragment>
   );
 };
 
