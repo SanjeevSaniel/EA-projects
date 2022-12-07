cc7c999477343b451ad62c6ef1fe163b70c0c0e7
SanjeevSaniel <sanjeevsaniel@outlook.in>
2022-12-07 15:56:30 +0530

    Build the Mobile Responsiveness

--- Portfolio-Page/index.js
+++ Portfolio-Page/index.js
@@ -7,12 +7,13 @@ window.onscroll = () => {
   previousScrollPosition = currentScrollPosition;
 };
 
-
 // Get the button:
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -27,16 +28,37 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-
 // Load More Abouts
-$(document).ready(function(){
+$(document).ready(function () {
   $(".content").slice(0, 3).show();
-  $("#loadMore").on("click", function(e){
+  $("#loadMore").on("click", function (e) {
     e.preventDefault();
     $(".content:hidden").slice(0, 3).slideDown();
-    if($(".content:hidden").length == 0) {
+    if ($(".content:hidden").length == 0) {
       // $("#loadMore").text("No Content").addClass("noContent");
       $("#loadMore").text("").addClass("noContent");
+      // $("#loadMore").text("").style.display = "none";
     }
   });
-})
+});
+
+const handleDropdownClicked = (event) => {
+  event.stopPropagation();
+  const dropdown = document.getElementById("hamburger");
+  toggleDropdown(!dropdown.classList.contains("open"));
+};
+
+const toggleDropdown = (shouldOpen) => {
+  const dropdown = document.getElementById("hamburger");
+  const icon = document.getElementById("icon");
+
+  if (shouldOpen) {
+    dropdown.classList.add("open");
+  } else {
+    dropdown.classList.remove("open");
+  }
+
+  icon.innerText = dropdown.classList.contains("open") ? "close" : "menu";
+};
+
+document.body.addEventListener("click", () => toggleDropdown());
