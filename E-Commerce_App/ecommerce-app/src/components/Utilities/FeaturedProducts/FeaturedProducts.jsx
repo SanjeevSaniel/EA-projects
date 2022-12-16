bd477923e813fdbf8be7120b54d7db06fe1e1046
SanjeevSaniel <sanjeevsaniel@outlook.in>
2022-12-17 00:47:35 +0530

    Build the Horizontal Scroll

--- E-Commerce_App/ecommerce-app/src/components/Utilities/FeaturedProducts/FeaturedProducts.jsx
+++ E-Commerce_App/ecommerce-app/src/components/Utilities/FeaturedProducts/FeaturedProducts.jsx
@@ -6,8 +6,20 @@ import Stack from "@mui/material/Stack";
 // import AirPodsMax from "./images/airpods-max-select-green-202011.png";
 
 const FeaturedProducts = () => {
-  const data = require("./data.json");
+  const data = require("../../api/FeaturedProducts.json");
   const [products, setProducts] = useState([]);
+
+  const handleLeftClick = (e) => {
+    const content = document.getElementById("content");
+    content.scrollLeft -= 200;
+    e.preventDefault();
+  };
+
+  const handleRightClick = (e) => {
+    const content = document.getElementById("content");
+    content.scrollLeft += 200;
+    e.preventDefault();
+  };
 
   useEffect(() => {
     setProducts(data);
@@ -17,38 +29,52 @@ const FeaturedProducts = () => {
     <React.Fragment>
       <div className="featured-products-container">
         <h2 className="featured-products-heading">Featured Products</h2>
-        <div className="featured-products-list">
-          {products.map((product, index) => {
-            return (
-              <a
-                key={index}
-                className="feautured-product-link"
-                href="/"
-                target={"_blank"}
-              >
-                <div className="featured-product">
-                  <img src={product.image} alt={product.name} />
-                  <div className="featured-product-info">
-                    <div className="featured-product-name">{product.name}</div>
+        <div className="featured-products-main-container">
+          <div className="left-box">
+            <button id="btn-left-arrow" onClick={handleLeftClick}>
+              Left
+            </button>
+          </div>
+          <div className="featured-products-list" id="content">
+            {products.map((product, index) => {
+              return (
+                <a
+                  key={index}
+                  className="feautured-product-link"
+                  href="/"
+                  target={"_blank"}
+                >
+                  <div className="featured-product">
+                    <img src={product.image} alt={product.name} />
+                    <div className="featured-product-info">
+                      <div className="featured-product-name">
+                        {product.name}
+                      </div>
 
-                    <Stack spacing={1}>
-                      <Rating
-                        name="half-rating-read"
-                        //   defaultValue={2.5}
-                        value={product.rating}
-                        precision={0.5}
-                        readOnly
-                      />
-                    </Stack>
-                    <div>
-                      <span className="new-cost">₹ {product.newPrice}</span>
-                      <span className="old-cost">₹ {product.oldPrice}</span>
+                      <Stack spacing={1}>
+                        <Rating
+                          name="half-rating-read"
+                          //   defaultValue={2.5}
+                          value={product.rating}
+                          precision={0.5}
+                          readOnly
+                        />
+                      </Stack>
+                      <div>
+                        <span className="new-cost">₹ {product.newPrice}</span>
+                        <span className="old-cost">₹ {product.oldPrice}</span>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </a>
-            );
-          })}
+                </a>
+              );
+            })}
+          </div>
+          <div className="right-box">
+            <button id="btn-right-arrow" onClick={handleRightClick}>
+              Right
+            </button>
+          </div>
         </div>
       </div>
     </React.Fragment>
